20c20241409df11e1113853e5c177dac3ece5764
jundu <jundu@71c7ba27-f65e-4535-8570-938fef6cceb7>
2007-12-30 13:30:00 +0000

    S2CreatorのJavadocを追加。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2dwr/trunk@6 71c7ba27-f65e-4535-8570-938fef6cceb7

--- s2dwr/src/main/java/org/seasar/dwr/creator/S2Creator.java
+++ s2dwr/src/main/java/org/seasar/dwr/creator/S2Creator.java
@@ -8,6 +8,29 @@ import org.seasar.framework.container.S2Container;
 import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 import org.seasar.framework.util.ClassUtil;
 
+/**
+ * DWRからS2Container上のコンポーネントを利用するためのクラスです。
+ * <p>
+ * dwr.xmlに設定を記述することで、 S2Container上にあるコンポーネントをDWRを経由してJavaScriptから使えるようにします。
+ * S2Creatorを利用するためには、 以下のようにdwr.xmlを設定します。
+ * </p>
+ * 
+ * <pre>
+ * &lt;dwr&gt;
+ *     &lt;init&gt;
+ *         &lt;creator id=&quot;s2&quot; class=&quot;org.seasar.dwr.creator.S2Creator&quot; /&gt;
+ *     &lt;/init&gt;
+ *     &lt;allow&gt;
+ *         &lt;create creator=&quot;s2&quot; javascript=&quot;Hello&quot;&gt;
+ *             &lt;param name=&quot;component&quot; value=&quot;HelloService&quot;/&gt;
+ *         &lt;/create&gt;
+ *         &lt;create creator=&quot;s2&quot; javascript=&quot;Echo&quot;&gt;
+ *             &lt;param name=&quot;class&quot; value=&quot;dwr.example.EchoService&quot;/&gt;
+ *         &lt;/create&gt;
+ *     &lt;/allow&gt;
+ * &lt;/dwr&gt;
+ * </pre>
+ */
 public class S2Creator extends AbstractCreator implements Creator {
 
     private static Log log = LogFactory.getLog(S2Creator.class);
@@ -52,6 +75,15 @@ public class S2Creator extends AbstractCreator implements Creator {
         return false;
     }
 
+    /**
+     * インスタンスを生成する対象のコンポーネントをクラスで指定します。
+     * <p>
+     * このプロパティを指定する場合は、 componentプロパティを指定していてはいけません。
+     * </p>
+     * 
+     * @param className
+     *                クラス名をFQNで指定
+     */
     public void setClass(String className) {
         if (componentName != null) {
             throw new IllegalStateException(
@@ -60,6 +92,15 @@ public class S2Creator extends AbstractCreator implements Creator {
         classType = ClassUtil.forName(className);
     }
 
+    /**
+     * インスタンスを生成する対象のコンポーネントをコンポーネント名で指定します。
+     * <p>
+     * このプロパティを指定する場合は、 classプロパティを指定していてはいけません。
+     * </p>
+     * 
+     * @param componentName
+     *                コンポーネント名
+     */
     public void setComponent(String componentName) {
         if (classType != null) {
             throw new IllegalStateException(
